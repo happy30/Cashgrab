df79f42230bd903f07615d9f16d9bc3e31e7c004
happy30 <jordivdh@hotmail.com>
2016-11-07 02:46:22 +0100

    o

--- MoveBlocks/Assets/AdManager.cs
+++ MoveBlocks/Assets/AdManager.cs
@@ -5,14 +5,23 @@ using GoogleMobileAds.Api;
 
 public class AdManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    BannerView bannerView;
+
+    float time = 0;
+
+    // Use this for initialization
+    void Start () {
         RequestBanner();
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        time += Time.deltaTime;
+        if(time > 60)
+        {
+            RequestBanner();
+            bannerView.Hide();
+        }   
 	}
 
 
@@ -20,10 +29,12 @@ public class AdManager : MonoBehaviour {
     // link to docs: https://firebase.google.com/docs/admob/unity/start
     private void RequestBanner()
     {
+        bannerView.OnAdLoaded -= BannerView_OnAdLoaded;
+
 #if UNITY_EDITOR
         string adUnitId = "unused";
 #elif UNITY_ANDROID
-        string adUnitId = "INSERT_ANDROID_BANNER_AD_UNIT_ID_HERE";
+        string adUnitId = "ca-app-pub-2780744705157681/9009802704";
 #elif UNITY_IPHONE
         string adUnitId = "INSERT_IOS_BANNER_AD_UNIT_ID_HERE";
 #else
@@ -31,10 +42,17 @@ public class AdManager : MonoBehaviour {
 #endif
 
         // Create a 320x50 banner at the top of the screen.
-        BannerView bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+        bannerView  = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
         // Create an empty ad request.
         AdRequest request = new AdRequest.Builder().Build();
         // Load the banner with the request.
         bannerView.LoadAd(request);
+
+        bannerView.OnAdLoaded += BannerView_OnAdLoaded;
+    }
+
+    private void BannerView_OnAdLoaded(object sender, System.EventArgs e)
+    {
+        bannerView.Show();
     }
 }
