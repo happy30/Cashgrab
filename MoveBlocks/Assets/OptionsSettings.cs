48645a5e147f9fe6c14164479146f8aa5feea4b0
happy30 <jordivdh@hotmail.com>
2016-12-23 15:32:21 +0100

    volumesettings (still has to be set ingame)

---
+++ MoveBlocks/Assets/OptionsSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsSettings : MonoBehaviour {
+
+    public float BGMFactor;
+    public float SFXFactor;
+
+}
