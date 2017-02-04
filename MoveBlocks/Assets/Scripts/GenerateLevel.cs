25af456789202c46fdec057603403b9000afbdf6
happy30 <jordivdh@hotmail.com>
2017-02-04 17:19:30 +0100

    5.6

--- MoveBlocks/Assets/Scripts/GenerateLevel.cs
+++ MoveBlocks/Assets/Scripts/GenerateLevel.cs
@@ -152,6 +152,30 @@ public class GenerateLevel : MonoBehaviour
                             colourTargetCounter++;
                         }
                     }
+                    else if (levelManager.levels[levelManager.currentLevel].colouredSeed == 4)
+                    {
+                        spawnedTile.GetComponent<SpriteRenderer>().sprite = greyTarget;
+                        if (colourTargetCounter == 0)
+                        {
+                            spawnedTile.GetComponent<SpriteRenderer>().color = Color.cyan;
+                            colourTargetCounter++;
+                        }
+                        else if (colourTargetCounter == 1)
+                        {
+                            spawnedTile.GetComponent<SpriteRenderer>().color = Color.yellow;
+                            colourTargetCounter++;
+                        }
+                        else if (colourTargetCounter == 2)
+                        {
+                            spawnedTile.GetComponent<SpriteRenderer>().color = Color.red;
+                            colourTargetCounter++;
+                        }
+                        else if (colourTargetCounter == 3)
+                        {
+                            spawnedTile.GetComponent<SpriteRenderer>().color = Color.magenta;
+                            colourTargetCounter++;
+                        }
+                    }
                 }
                 else if (level.GetPixel(x, y).g > 0.3f && level.GetPixel(x, y).g < 0.7f)
                 {
